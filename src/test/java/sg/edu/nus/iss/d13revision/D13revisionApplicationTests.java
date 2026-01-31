72dfec62d8e9e342c3f9ddc7466789eed4371754
WEBSPARKS\Michael.winson <michael.winson@websparks.sg>
2026-01-31 14:57:32 +0800

    remove test

--- src/test/java/sg/edu/nus/iss/d13revision/D13revisionApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/d13revision/D13revisionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class D13revisionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
